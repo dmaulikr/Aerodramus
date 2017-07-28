2393414db6cb8aa22492ef43cf4b8800b31e4e84
xiaolong.jin <xiaolong.jin@icloud.com>
2017-07-28 10:41:59 +0800

    init project

---
+++ Aerodramus/Aerodramus/Aerodramus.h
@@ -0,0 +1,19 @@
+//
+//  Aerodramus.h
+//  Aerodramus
+//
+//  Created by 金晓龙 on 2017/7/28.
+//  Copyright © 2017年 Zodiac.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Aerodramus.
+FOUNDATION_EXPORT double AerodramusVersionNumber;
+
+//! Project version string for Aerodramus.
+FOUNDATION_EXPORT const unsigned char AerodramusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aerodramus/PublicHeader.h>
+
+
---
+++ Aerodramus/Aerodramus/Callbacks/Callbacks.swift
@@ -0,0 +1,15 @@
+//
+//  Callbacks.swift
+//  Aerodramus
+//
+//  Created by 金晓龙 on 2017/7/28.
+//  Copyright © 2017年 Zodiac.com. All rights reserved.
+//
+
+import Foundation
+
+public struct Callbacks {
+    
+    
+    
+}
